623b87845d8e814b3830def4473756b1b6b1e829
Steve Hsiao <steve550610@hotmail.com>
2020-01-02 10:48:23 +0800

    add if 判斷

--- app.py
+++ app.py
@@ -37,9 +37,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    if msg in ['hi' ,'Hi' ,'HI'] :
+        r = '嗨'
+    elif msg == '你是誰' :
+        r = '我是機器人'
+    elif '訂位' in msg :
+        r = '請撥訂位電話 3388'
+    else :
+        r = '你說什麼'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
